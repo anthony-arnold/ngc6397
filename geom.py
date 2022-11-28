ff0ffd6f58901924f5eb9204b448cf0c316da354
Anthony Arnold <anthony.arnold@uqconnect.edu.au>
2022-11-28 20:32:56 +1000

    Add Trager error bars.

--- geom.py
+++ geom.py
@@ -6,13 +6,13 @@ def rad2arcsec(rad):
 def deg2rad(deg):
     return deg * 0.01745329
 
-def angular_dist(data, ra, dec):
+def angular_dist(ra, dec, cra, cdec):
     """
     Get the angular distance of each data point from the given 
     celestial coordinates. All angles are in degrees and the result
     is in arcsec.
     """
-    dra = deg2rad(ra - data['ra']) * np.cos(deg2rad(dec))
-    ddec = deg2rad(dec - data['dec'])
+    dra = deg2rad(cra - ra) * np.cos(deg2rad(cdec))
+    ddec = deg2rad(cdec - dec)
 
     return np.sqrt(dra**2 + ddec**2)
